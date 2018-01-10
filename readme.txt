bea21f6d3b8958fa22ef58c61e273dda42b3857f
root <root@ip-172-31-33-143.eu-west-1.compute.internal>
2018-01-10 09:20:45 +0000

    prj custom

---
+++ readme.txt
@@ -0,0 +1,1 @@
+creaturum Crypto Coin
---
+++ src/P2p/P2pNetworks.h
@@ -0,0 +1,10 @@
+// Copyright (c) 2011-2016 The Cryptonote developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#pragma once
+
+namespace CryptoNote
+{
+  const static boost::uuids::uuid CRYPTONOTE_NETWORK = { { 0x71, 0x06, 0x13, 0x49, 0x98, 0x70, 0x4b, 0x4a, 0x4c, 0x50, 0x60, 0x70, 0x2a, 0x2b, 0x2c, 0x2e } };
+}
